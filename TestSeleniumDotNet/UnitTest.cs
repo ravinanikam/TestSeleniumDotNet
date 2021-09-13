724a19ec481ea01d060b73f78b340eef6e8ee890
ravinanikam <ravinad74@gmail.com>
2021-09-13 22:51:46 +0100

    Initial commit
    Testing the website using custom control and creating the driver helper class.

---
+++ TestSeleniumDotNet/UnitTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace TestSeleniumDotNet
+{
+    public class UnitTest : DriverHelper
+    {
+        //public  IWebDriver Driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Console.WriteLine("SetUp");
+            Driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com");
+            Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+            Driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
+
+            //string comboControlName = "ContentPlaceHolder1_AllMealsCombo";
+            //CustomControl customControl = new CustomControl();
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Buckwheat");
+            Console.WriteLine("First Test");
+            Assert.Pass();
+        }
+    }
+}
